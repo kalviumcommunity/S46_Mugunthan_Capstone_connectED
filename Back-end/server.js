943d5337d1e1ab54fc4346617cf159fa4161a2f2
Mugunthan <mugunthan701@gmail.com>
2024-04-05 12:00:36 +0530

    mongo connection added

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,12 +1,38 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config()
 const app = express();
-const port =9001;
+const port = 9001;
+
+const startDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.URI);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1);
+  }
+};
+
+startDatabase();
+
+const isConnected = () => {
+  return mongoose.connection.readyState === 1;
+};
+
+const checkDatabaseConnection = (req, res, next) => {
+  if (!isConnected()) {
+    return res.status(500).json({ message: "Database is not connected" });
+  }
+  next(); // Call next to move to the next middleware or route handler
+};
+
+app.use(checkDatabaseConnection); // Apply the middleware globally
 
 app.get("/", (req, res) => {  
-    res.json({message: 'Initiated backend'});
-    // res.json({ hello: "hello" });
-  });
+  res.json({ message: 'Initiated backed' });
+});
 
-app.listen(port,()=>{
-  console.log(`server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
